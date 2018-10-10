f07b43a5d7cc82a89d27947fb410db2c5a8fff77
DenisRam <sonydenissony@gmail.com>
2018-10-11 01:34:44 +0300

    1

---
+++ src/CarOptions.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+
+public class CarOptions {
+
+    public static ArrayList <Car> showCarsByBrand(ArrayList <Car> cars){
+        return null;
+    }
+
+    public static ArrayList <Car> showCarsByModelRideByYears(ArrayList <Car> cars){
+        return null;
+    }
+
+    public static ArrayList <Car> showCarsByYearWithLargerPrice(ArrayList <Car> cars){
+        return null;
+    }
+}
