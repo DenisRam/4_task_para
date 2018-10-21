32040bd518f9bc4ad43f5f0d19dc0fd1a98bca4b
Chameleonchik <grabar.nikolay@gmail.com>
2018-10-21 15:08:44 +0300

    Update CarOptions

--- src/CarOptions.java
+++ src/CarOptions.java
@@ -3,15 +3,17 @@ import java.util.ArrayList;
 
 public class CarOptions {
 
-    public static ArrayList <Car> showCarsByBrand(ArrayList <Car> cars){
+    public static ArrayList<Car> showCarsByBrand(ArrayList<Car> cars) {
+        ArrayList<Car> car = new ArrayList<Car>();
+        //cars.stream().filter(car -> car.getPrice()>500);
+        return car;
+    }
+
+    public static ArrayList<Car> showCarsByModelRideByYears(ArrayList<Car> cars) {
         return null;
     }
 
-    public static ArrayList <Car> showCarsByModelRideByYears(ArrayList <Car> cars){
-        return null;
-    }
-
-    public static ArrayList <Car> showCarsByYearWithLargerPrice(ArrayList <Car> cars){
+    public static ArrayList<Car> showCarsByYearWithLargerPrice(ArrayList<Car> cars) {
         return null;
     }
 }
--- src/MainCars.java
+++ src/MainCars.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class MainCars {
     public static void main(String[] args) {
-        System.out.println("Введите количество машин");
+        System.out.println("Введите количество машин:");
         Scanner sc = new Scanner(System.in);
         ArrayList <Car> cars  = CarFactory.carPark(sc.nextInt());
 
         System.out.println(cars);
 
-}
+    }
 }
